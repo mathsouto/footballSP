cc476128540f7788883a35f11f14f16e10675a45
Matheus <tetisouto@gmail.com>
2024-05-23 15:19:18 -0300

    Novo menu e correções de responsivo

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-const menuIcon = document.getElementById('menuIcon');
-const headerNav = document.querySelector('.headerNav');
-let menuOpen = false;
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menuIcon');
+    const headerNav = document.querySelector('.headerNav');
+    const overlay = document.getElementById('overlay');
 
-menuIcon.addEventListener('click', function() {
-    menuOpen = !menuOpen;
-    headerNav.classList.toggle('show', menuOpen);
-    menuIcon.src = menuOpen ? './assets/icons/menuIcon2.svg' : './assets/icons/menuIcon.svg';
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    // Função para alternar a visibilidade do menu e do overlay
+    menuIcon.addEventListener('click', function(event) {
+        event.stopPropagation(); // Impede o clique de propagação para o documento
+        headerNav.classList.toggle('show');
+        overlay.classList.toggle('show');
+    });
+
+    // Função para fechar o menu ao clicar fora
+    document.addEventListener('click', function(event) {
+        const isClickInsideMenu = headerNav.contains(event.target);
+        const isClickOnMenuIcon = menuIcon.contains(event.target);
+        
+        if (!isClickInsideMenu && !isClickOnMenuIcon) {
+            closeMenu();
+        }
+    });
+
+    // Função para fechar o menu ao rolar a página
+    document.addEventListener('scroll', function() {
+        closeMenu();
+    });
+
+    // Fechar o menu ao clicar no overlay
+    overlay.addEventListener('click', function() {
+        closeMenu();
+    });
+
+    // Detectar gesto de arrastar no celular
+    document.addEventListener('touchstart', function(event) {
+        touchStartX = event.touches[0].clientX;
+    });
+
+    document.addEventListener('touchmove', function(event) {
+        touchEndX = event.touches[0].clientX;
+    });
+
+    document.addEventListener('touchend', function(event) {
+        if (touchEndX - touchStartX > 50) {
+            closeMenu();
+        }
+    });
+
+    // Função para fechar o menu
+    function closeMenu() {
+        headerNav.classList.remove('show');
+        overlay.classList.remove('show');
+    }
 });
