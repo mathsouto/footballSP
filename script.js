e5eac6a6530806693f3871dffc2dd54fa7f7cac0
Matheus <tetisouto@gmail.com>
2024-05-24 00:12:56 -0300

    Adicionado o efeito digitando.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const menuIcon = document.getElementById('menuIcon');
 const headerNav = document.querySelector('.headerNav');
 const overlay = document.getElementById('overlay');
+const sectionOneH1 = document.querySelector('.sectionOneH1');
 
 let touchStartX = 0;
 let touchEndX = 0;
+
+// Função para criar o efeito de digitação do título da sectionOne
+function typeWriter(sectionOneH1) {
+    const textoOriginal = sectionOneH1.innerHTML;
+    sectionOneH1.innerHTML = '';
+    
+    let i = 0;
+    const interval = setInterval(() => {
+        if (i < textoOriginal.length) {
+            sectionOneH1.innerHTML += textoOriginal.charAt(i);
+            i++;
+        } else {
+            clearInterval(interval);
+            setTimeout(() => {
+                typeWriter(sectionOneH1); // Reinicia o efeito após um intervalo
+            }, 1000); // Tempo de espera antes de reiniciar
+        }
+    }, 100); // Velocidade da digitação
+}
+typeWriter(sectionOneH1);
+
 
 // Função para alternar a visibilidade do menu e do overlay
 menuIcon.addEventListener('click', function(event) {
